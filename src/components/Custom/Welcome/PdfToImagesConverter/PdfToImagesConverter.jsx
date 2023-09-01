81ccfe41d1e2a930384c69054227778ba3a5c728
Ahmed Mahmoud <ahmedmahmoudtech@gmail.com>
2023-09-01 17:51:20 +0300

    display init

--- src/components/Custom/Welcome/PdfToImagesConverter/PdfToImagesConverter.jsx
+++ src/components/Custom/Welcome/PdfToImagesConverter/PdfToImagesConverter.jsx
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
+// import { ipcRenderer } from 'electron';
+function FileInputExample() {
+  const [file, setFile] = useState(null);
 
-function PdfToImagesConverter() {
-  return <div></div>;
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  // const handleUpload = () => {
+  //   if (file) {
+  //     ipcRenderer.send('upload-request', file);
+  //   }
+  // };
+
+  return (
+    <div className="App">
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload and Convert</button>
+    </div>
+  );
 }
 
-export default PdfToImagesConverter;
+export default FileInputExample;
